ed830cfeb9216e199ba0ad6e9de2290a70746114
Probal Das <49045586+probaldas@users.noreply.github.com>
2024-12-01 13:34:19 -0500

    Adding apache beam and google cloud dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.beam:beam-sdks-java-core:2.61.0")
+    implementation("org.apache.beam:beam-sdks-java-io-google-cloud-platform:2.61.0")
+    implementation("org.apache.beam:beam-runners-google-cloud-dataflow-java:2.61.0")
+    implementation("com.google.cloud:google-cloud-storage:2.22.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
