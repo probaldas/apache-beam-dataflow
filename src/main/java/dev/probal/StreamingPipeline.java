986bd052e6ade0851481b93f6810d7ce601b9325
Probal Das <49045586+probaldas@users.noreply.github.com>
2024-12-02 22:41:56 -0500

    Changed input data. Used zillow.csv as input data. Implemented logic on exception handling due to bad data.

--- src/main/java/dev/probal/StreamingPipeline.java
+++ src/main/java/dev/probal/StreamingPipeline.java
@@ -2,23 +2,36 @@ package dev.probal;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
-import org.apache.beam.sdk.values.PDone;
+import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.TupleTagList;
 
 public class StreamingPipeline {
-    private static final String INPUT_BUCKET = "gs://apache-beam-samples/shakespeare/kinglear.txt";
-    private static final String DESTINATION_BUCKET = "gs://beam-prj-data/beam-prj-data/dataflow/transformed-kinglear.txt";
+    private static final String BUCKET_PATH = "gs://beam-prj-data/beam-prj-data/dataflow/";
+    private static final String INPUT_BUCKET_FILE_NAME = "zillow.csv";
+    private static final String VALID_BUCKET_FILE_NAME = "zillow-results.txt";
+    private static final String FAILURE_BUCKET_FILE_NAME = "zillow-unknown.txt";
 
     public static void main(String[] args) {
         Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
         Pipeline p = Pipeline.create(options);
 
-        PCollection<String> input = p.apply("Read", TextIO.read().from(INPUT_BUCKET));
-        PCollection<String> transformedData = input.apply("Transform", ParDo.of(new Transformation()));
-        PDone done = transformedData.apply("Write", TextIO.write().to(DESTINATION_BUCKET));
+        PCollection<String> inputData = p.apply("Read", TextIO.read().from(BUCKET_PATH + INPUT_BUCKET_FILE_NAME));
+
+        PCollectionTuple transformedTuple = inputData.apply("Transform", ParDo.of(new Transformation())
+                .withOutputTags(Transformation.VALID_DATA_TAG, TupleTagList.of(Transformation.FAILURE_DATA_TAG)));
+
+        transformedTuple.get(Transformation.VALID_DATA_TAG)
+                .setCoder(StringUtf8Coder.of())
+                .apply("Save Result", TextIO.write().to(BUCKET_PATH + VALID_BUCKET_FILE_NAME));
+
+        transformedTuple.get(Transformation.FAILURE_DATA_TAG)
+                .setCoder(StringUtf8Coder.of())
+                .apply("Failure Result", TextIO.write().to(BUCKET_PATH + FAILURE_BUCKET_FILE_NAME));
 
         PipelineResult result = p.run();
         try {
--- src/main/java/dev/probal/Transformation.java
+++ src/main/java/dev/probal/Transformation.java
@@ -1,13 +1,36 @@
 package dev.probal;
 
 import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.values.TupleTag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Transformation extends DoFn<String, String> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Transformation.class);
+
+    public static TupleTag<String> VALID_DATA_TAG = new TupleTag<>();
+    public static TupleTag<String> FAILURE_DATA_TAG = new TupleTag<>();
+
     @ProcessElement
     public void processElement(ProcessContext c) {
-        String line = c.element();
-        line = line.toUpperCase();
+        String row = c.element();
+        LOG.info("Row: " + row);
 
-        c.output(line);
+        String[] items = row.split(",");
+
+        try {
+            int sqft = Integer.parseInt(items[1].trim());
+            int price = Integer.parseInt(items[6].trim());
+            float pricePerSqft = price/sqft;
+
+            String message = "Zip Code: " + items[4].trim() + " house cost is $" + String.valueOf(pricePerSqft) + " per sqft";
+            LOG.info(message);
+            c.output(VALID_DATA_TAG, message);
+        } catch (Exception e) {
+            String message = "Zip Code: " + items[4].trim() + " house\'s cost is UNKNOWN";
+            LOG.info(message);
+            c.output(FAILURE_DATA_TAG, message);
+        }
     }
 }
